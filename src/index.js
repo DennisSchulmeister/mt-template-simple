83aedde447230f36feda662217fd8d724ab74188
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-05 22:26:09 +0200

    Added missing ; in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import MiniTutorial from "mini-tutorial.js";
-import mtTheme from "mini-tutorial.js/themes/white.css"
+import mtTheme from "mini-tutorial.js/themes/white.css";
 
 window.addEventListener("load", () => {
     let mt = new MiniTutorial();
